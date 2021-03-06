d2857b63a7c9b1800f8912b1dc4cef63df9f7932
Vukašin Stepanović <vukasinstepanovic@gmail.com>
2021-03-06 15:25:22 +0100

    feat: Use seeded RNG

--- src/players.ts
+++ src/players.ts
@@ -1,13 +1,13 @@
 import { XY } from "./chain_reaction";
 import { Player } from "./runner";
-import { waitForEvent } from "./util";
+import { random, waitForEvent } from "./util";
 
 export const PlayRandomly: Player = {
 	play({ width, height, canPlace }) {
 		// eslint-disable-next-line no-constant-condition
 		while (true) {
-			const x = Math.floor(width * Math.random());
-			const y = Math.floor(height * Math.random());
+			const x = Math.floor(width * random());
+			const y = Math.floor(height * random());
 
 			if (canPlace(x, y)) {
 				return { x, y };
--- src/util.ts
+++ src/util.ts
@@ -27,3 +27,18 @@ export function waitForEvent<
 		element.addEventListener(type, resolve, { once: true });
 	});
 }
+
+export function mulberry32(seed: number): () => number {
+	let a = seed;
+
+	return function random() {
+		a |= 0;
+		a = (a + 0x6d2b79f5) | 0;
+		let t = Math.imul(a ^ (a >>> 15), 1 | a);
+		t = (t + Math.imul(t ^ (t >>> 7), 61 | t)) ^ t;
+		return ((t ^ (t >>> 14)) >>> 0) / 4294967296;
+	};
+}
+
+const seed = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER);
+export const random = mulberry32(seed);
